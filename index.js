4e6a47c4a8ebe14bb351bb38634b266b12014237
Abhinav068 <abhinav28897@gmail.com>
2024-04-17 00:08:13 +0530

    all endpoints created

--- index.js
+++ index.js
@@ -13,26 +13,49 @@ app.get('/', async (req, res) => {
     res.send('serviceNow api');
 })
 
-app.post('/', async (req, res) => {
+app.post('/users', async (req, res) => {
     try {
         const { ID, FirstName, LastName, Department } = req.body;
         const user = await UserModel({ ID, FirstName, LastName, Department });
         await user.save();
         res.send({ msg: 'User Successfully registered.' });
-        
+
     } catch (error) {
-        res.send({msg:'Something went wrong'});
+        res.send({ msg: 'Something went wrong' });
     }
 })
 
-app.get('/users',async (req,res) => {
+app.get('/users', async (req, res) => {
     try {
         const users = await UserModel.find();
-        res.send({users});
-        
+        res.send(users);
+
     } catch (error) {
-        res.send({msg:'Something went wrong'});
-        
+        res.send({ msg: 'Something went wrong' });
+
+    }
+})
+app.patch('/users/:ID', async (req, res) => {
+    try {
+        const ID = req.params.ID;
+        const payload = req.body;
+        const doc = await UserModel.findOneAndUpdate({ ID }, { payload }, { new: true });
+        res.send(users);
+
+    } catch (error) {
+        res.send({ msg: 'Something went wrong' });
+
+    }
+})
+app.delete('/users/:ID', async (req, res) => {
+    try {
+        const ID = req.params.ID;
+        const doc = await UserModel.findOneAndDelete({ ID });
+        res.send(users);
+
+    } catch (error) {
+        res.send({ msg: 'Something went wrong' });
+
     }
 })
 
